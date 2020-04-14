5696a7e8ae6c08cc6272290f6d4c7e5fffc4e10b
尹庆文 <yinqingwen@hotmail.com>
2020-04-14 21:50:02 +0800

    修改校验逻辑

--- Samples/netcore3.0-mvc/Senparc.Weixin.Sample.NetCore3/wwwroot/js/Validate.js
+++ Samples/netcore3.0-mvc/Senparc.Weixin.Sample.NetCore3/wwwroot/js/Validate.js
@@ -26,37 +26,33 @@ var Validate = {
     },
     //电话号码校验
     phone: function (elem, errmsg) {
-        var result = true;
         var value = $.trim($(elem).val());
         if (!/^((0\d{2,3}-\d{7,8})|(\d{7,8})|(1[3456789]\d{9}))$/.test(value)) { // /^(0|86|17951)?(13[0-9]|15[012356789]|18[0-9]|14[57]|17[0-9])[0-9]{8}$/
-            $.toptip(errmsg);
-            //$(elem).focus();
-            result = false;
+            return false;
+        } else {
+            return true;
         }
-        return result;
     },
+
     //Email校验
     email: function (elem, errmsg) {
         var result = true;
         var value = $.trim($(elem).val());
         if (!/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/.test(value)) {
-            $toptip(errmsg);
-            //$(elem).focus();
-            result = false;
+            return false;
+        } else {
+            return true;
         }
-        return result;
     },
     //相同校验
     compare: function (elem1, elem2, errmsg) {
-        var result = true;
         elem2 = $(elem2);
         var value1 = $.trim($(elem1).val());
         var value2 = $.trim($(elem2).val());
         if (value1 !== value2) {
-            $.toptip(errmsg);
-            //$(elem2).focus();
-            result = false;
+            return false;
+        } else {
+            return true;
         }
-        return result;
     }
 };
